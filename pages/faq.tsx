cb364c4589602a8c248fc356ab8aa773f3e099c3
BPL <BPLFamily@gmail.com>
2022-01-16 16:27:37 +0100

    Added FAQs

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -11,116 +11,77 @@ export default function FAQ(): ReactElement {
   return (
     <Layout>
       <div className="container mx-auto py-20 px-4 sm:w-1/2">
-        <h2>Frequently Asked Questions</h2>
+        <h2>FAQs</h2>
 
         {/* What is loot ? */}
         <div className="my-8">
-          <h3>What is Loot?</h3>
+          <h3>What is 1501?</h3>
           <p>
-            Loot is a collection of 8,000 unique bags of adventurer gear,
-            originally released by{" "}
+            
+The 1501 Project is a randomized collection from the famous speech "I Have a Dream" by Martin Luther King Jr. 
             <a
               href="https://twitter.com/dhof/status/1431316631934967815"
               target="_blank"
               rel="noopener noreferrer"
             >
-              Dom Hofmann
+              Adebaoy
             </a>
-            . At release, anyone could claim loot bags for just gas, and all
-            bags were claimed in under 4 hours. Each loot bag contains 8 items:
-            a piece for an adventurer&apos;s chest, foot, hand, head, neck, ring,
-            waist, and weapon.
+            Our goal is to celebrate the contributions of the MLKJ in the Civil Rights Movement and emphasize the idea of Freedom as the most fundamental Essence of Humanity.
           </p>
-          <p>
-            Loot is an unaudited project. Bags #1 to #7777 were claimable by
-            anyone and #7778 to #8000 are currently reserved for the contract
-            deployer.
-          </p>
+ 
         </div>
 
-        {/* Why is loot special? */}
-        <div className="my-8">
-          <h3>Why is loot special?</h3>
-          <p>
-            Loot is unique—the first project of its kind. With no company, art,
-            team, or attributes, Loot makes it impossible to gate-keep any
-            creative decisions (h/t{" "}
-            <a
-              href="https://twitter.com/john_c_palmer/status/1432606797186179072?s=20"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              @john_c_palmer
-            </a>
-            ).
-          </p>
-          <p>
-            Loot is the unfiltered, uncensorable building block for stories,
-            experiences, games, and more, in the hands of the community, at no
-            cost. Loot pursues complete decentralization from day one.
-          </p>
-        </div>
+      
 
         {/* Can I build with loot? */}
         <div className="my-8">
-          <h3>Can I build with Loot?</h3>
+          <h3>What is the total supply of the project?</h3>
           <p>
-            Yes, you are free to use Loot in any way you want. For inspiration,
-            see existing{" "}
-            <Link href="/resources">
-              <a>resources</a>
-            </Link>{" "}
-            put together by the community.
+           1501 Nfts
           </p>
         </div>
 
         {/* Am I priced out of loot? */}
         <div className="my-8">
-          <h3>Am I priced out of loot?</h3>
+          <h3>How do I buy myself a 1501?</h3>
           <p>
-            Not at all. Through{" "}
-            <Link href="/synthloot">
-              Synthetic Loot
-            </Link>
-            , all addresses have access to virtual Loot that developers can
-            integrate into the Loot projects they build.
-          </p>
-          <p>
-            Thus, anyone with an Ethereum wallet is allowed to participate in
-            the ecosystem, while still maintaining distinction between original
-            Loot and synthetics.
+           The project launch is on 17th Jan on the day of the MLKJ birthday celebration. 
           </p>
         </div>
 
+
+<div className="my-8">
+          <h3>What Solana wallets can I use?</h3>
+          <p>
+   We recommend using Phantom wallet. But you will be able to use Phantom, Sollet or Solflare.
+          </p>
+        </div>
+
+ {/* Am I priced out of loot? */}
+        <div className="my-8">
+          <h3>How to check the rarity of my 1501?</h3>
+          <p>
+
+We will release the rarity ranking and a rarity tools to figure out the rarities.  
+
+     </p>
+        </div>
+        
         {/* How do I value loot bags? */}
         <div className="my-8">
-          <h3>How do I value Loot bags?</h3>
+          <h3>Is there an aftermarket sales royalty?</h3>
           <p>
-            They say that value is always in the eye of the beholder. Loot is no
-            different, with no explicit rarities specified at launch. How you
-            value a loot bag is up to you.
+            Yes. There is a 5% royalties on each sale to support the project long term and providing a free launchpad platform to young artists! The royalties division is as follows
+
           </p>
           <p>
-            Still, the community has begun to devise many mechanisms by which to
-            assess the rarity of bags and their items. Some of these include{" "}
-            <a
-              href="https://github.com/Anish-Agnihotri/dhof-loot/blob/master/output/rare.json"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              rarity by occurence
-            </a>{" "}
-            or{" "}
-            <a
-              href="https://0xinventory.app/help"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              item score
-            </a>
-            .
+          
+          <ul>
+  <li> - 3% to launchpad and black art promotion <br/></li>
+  <li> - 2% to the dev team wallet</li>
+</ul>
+  
           </p>
-          <p>Remember, use your own discretion when valuing a loot bag.</p>
         </div>
       </div>
     </Layout>
